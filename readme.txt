a1fd56988ab8cf1e4f5e2d3099899e4dec74c4c9
Frankie Jarrett <frankie@x-team.com>
2015-05-05 14:35:29 -0500

    Repo setup

---
+++ readme.txt
@@ -0,0 +1,26 @@
+=== User Registration Control ===
+Contributors:      fjarrett
+Tags:              admin, membership, registration, security, users
+Requires at least: 3.7
+Tested up to:      4.2
+Stable tag:        0.1.0
+License:           GPLv3
+License URI:       http://www.gnu.org/licenses/gpl-3.0.html
+
+Dynamic control over who can register on your site and when.
+
+== Description ==
+
+**Did you find this plugin helpful? Please consider [writing a review](https://wordpress.org/support/view/plugin-reviews/prevent-concurrent-logins).**
+
+Something here.
+
+**Development of this plugin is done [on GitHub](https://github.com/fjarrett/prevent-concurrent-logins). Pull requests welcome. Please see [issues reported](https://github.com/fjarrett/prevent-concurrent-logins/issues) there before going to the plugin forum.**
+
+== Changelog ==
+
+= 0.1.0 - May 7, 2015 =
+
+* Initial release
+
+Props [fjarrett](https://profiles.wordpress.org/fjarrett/)
---
+++ user-registration-control.php
@@ -0,0 +1,80 @@
+<?php
+/**
+ * Plugin Name: User Registration Control
+ * Description: Dynamic control over who can register on your site and when.
+ * Version: 0.1.0
+ * Author: Frankie Jarrett
+ * Author URI: http://frankiejarrett.com
+ * License: GPLv3
+ * Text Domain: user-registration-control
+ */
+
+class User_Registration_Control {
+
+	/**
+	 * Plugin version number
+	 *
+	 * @const string
+	 */
+	const VERSION = '0.1.0';
+
+	/**
+	 * Hold plugin instance
+	 *
+	 * @var string
+	 */
+	public static $instance;
+
+	/**
+	 * Class constructor
+	 */
+	private function __construct() {
+		define( 'USER_REGISTRATION_CONTROL_PLUGIN', plugin_basename( __FILE__ ) );
+		define( 'USER_REGISTRATION_CONTROL_DIR', plugin_dir_path( __FILE__ ) );
+		define( 'USER_REGISTRATION_CONTROL_URL', plugin_dir_url( __FILE__ ) );
+		define( 'USER_REGISTRATION_CONTROL_INC_DIR', USER_REGISTRATION_CONTROL_DIR . 'includes/' );
+		define( 'USER_REGISTRATION_CONTROL_LANG_PATH', dirname( USER_REGISTRATION_CONTROL_PLUGIN ) . '/languages' );
+
+		add_action( 'plugins_loaded', array( __CLASS__, 'i18n' ) );
+		add_action( 'init', array( __CLASS__, 'load' ) );
+	}
+
+	/**
+	 * Load languages
+	 *
+	 * @action plugins_loaded
+	 *
+	 * @return void
+	 */
+	public static function i18n() {
+		load_plugin_textdomain( 'user-registration-control', false, USER_REGISTRATION_CONTROL_LANG_PATH );
+	}
+
+	/**
+	 * Register hooks
+	 *
+	 * @action init
+	 *
+	 * @return void
+	 */
+	public static function load() {
+		// Hooks here
+	}
+
+	/**
+	 * Return active instance of User_Registration_Control, create one if it doesn't exist
+	 *
+	 * @return User_Registration_Control
+	 */
+	public static function get_instance() {
+		if ( empty( self::$instance ) ) {
+			$class = __CLASS__;
+			self::$instance = new $class;
+		}
+
+		return self::$instance;
+	}
+
+}
+
+$GLOBALS['user_registration_control'] = User_Registration_Control::get_instance();
